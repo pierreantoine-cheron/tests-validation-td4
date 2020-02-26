e5583c4b241acbd7dca0fc2b6626b7f70bd511a0
Pierre-Antoine <pierre-antoine.cheron@etu.umontpellier.fr>
2020-02-26 12:05:51 +0100

    handle exception with return null

--- td4/src/main/java/testvalidation/App.java
+++ td4/src/main/java/testvalidation/App.java
@@ -1,33 +1,33 @@
 package testvalidation;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        int [] tab= racineCarrees(15, 17);
+public class App {
+    public static void main(String[] args) {
+        int[] tab = racineCarrees(5, 1);
         for (int i = 0; i < tab.length; i++) {
-            System.out.println(tab[i]+"\n");
+            System.out.println(tab[i] + "\n");
         }
     }
 
-    public static int[] racineCarrees(int a, int b) {//calcule les racines carrees des nombres de A a B
+    public static int[] racineCarrees(int a, int b) {// calcule les racines carrees des nombres de A a B
         try {
-            if (a>b) {
-                throw new Exception("A must be <= B");//renvoie une exception si b > a
+            if (a > b) {
+                throw new Exception("A must be <= B");// renvoie une exception si b > a
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            return null;
         }
         try {
-            if (a<0 || b<0) {
-                throw new Exception("A & Bmust be > 0");//renvoie une exception si b > a
+            if (a < 0 || b < 0) {
+                throw new Exception("A & Bmust be > 0");// renvoie une exception si b > a
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            return null;
         }
-        int[] tab = new int[b-a+1];
+        int[] tab = new int[b - a + 1];
         for (int i = a; i <= b; i++) {
-            tab[i-a] = (int)Math.sqrt(i);
+            tab[i - a] = (int) Math.sqrt(i);
         }
         return tab;
     }
