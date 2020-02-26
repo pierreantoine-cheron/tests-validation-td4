a0bcb4b0053656bffe49dde90c1b92426243debb
Pierre-Antoine <pierre-antoine.cheron@etu.umontpellier.fr>
2020-02-26 11:35:01 +0100

    v2

--- td4/src/main/java/testvalidation/App.java
+++ td4/src/main/java/testvalidation/App.java
@@ -5,11 +5,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int [] tab= racineCarrees(10, 15);
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]+"\n");
+        }
     }
 
-    public int[] racineCarrees(int a, int b) {//calcule les racines carrees des nombres de A a B
-        if (a>b) {
-            throw new Exception("A must be <= B");//renvoie une exception si b > a
+    public static int[] racineCarrees(int a, int b) {//calcule les racines carrees des nombres de A a B
+        try {
+            if (a>b) {
+                throw new Exception("A must be <= B");//renvoie une exception si b > a
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
         int[] tab = new int[b-a];
         for (int i = a; i < b; i++) {
