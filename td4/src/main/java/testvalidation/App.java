62e5530dc12816a9c6ca0a3eb8c6f2f5cd786d37
Pierre-Antoine <pierre-antoine.cheron@etu.umontpellier.fr>
2020-02-26 11:28:00 +0100

    racine carrees

--- td4/src/main/java/testvalidation/App.java
+++ td4/src/main/java/testvalidation/App.java
@@ -1,13 +1,21 @@
 package testvalidation;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+    public int[] racineCarrees(int a, int b) {//calcule les racines carrees des nombres de A a B
+        if (a>b) {
+            throw new Exception("A must be <= B");//renvoie une exception si b > a
+        }
+        int[] tab = new int[b-a];
+        for (int i = a; i < b; i++) {
+            tab[i-a] = (int)Math.sqrt(i);
+        }
+        return tab;
+    }
+
 }
