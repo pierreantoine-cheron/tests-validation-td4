6486be27e4886055d5166ecf7be12b2ca2ea65bc
ezzaamarin <a>
2020-02-26 11:54:30 +0100

    début des tests

--- td4/src/test/java/testvalidation/AppTest.java
+++ td4/src/test/java/testvalidation/AppTest.java
@@ -1,20 +1,58 @@
 package testvalidation;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+
+    App a;
+
+    @Before
+    public void setup() {
+        App a = new App();
     }
+
+    @Test
+    public void testPositif(){
+        int[] test = {4, 4, 4, 4, 4, 5, 5, 5, 5, 5};
+        assertArrayEquals(test, a.racineCarrees(20, 30));
+    }
+
+    @Test
+    public void testNbrNegatif(){
+        try {
+            a.racineCarrees(-5, -1);
+            assertTrue(false);
+        }
+        catch (Exception e){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testAplusgrandqueB(){
+        try {
+            a.racineCarrees(5, 1);
+            assertTrue(false);
+        }
+        catch (Exception e){
+            assertTrue(true);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
